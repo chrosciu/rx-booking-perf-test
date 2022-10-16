1b8416090100f16b49502815af44d30b039660df
Marcin Chrost <mchrost82@gmail.com>
2022-10-16 23:09:50 +0200

    Initial revision

---
+++ rx-booking/src/main/java/com/chrosciu/rxbooking/RxBookingApplication.java
@@ -0,0 +1,13 @@
+package com.chrosciu.rxbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RxBookingApplication.class, args);
+    }
+
+}
