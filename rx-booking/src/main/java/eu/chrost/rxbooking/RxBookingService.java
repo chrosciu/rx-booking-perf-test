f2848818502005e65aefd818b4c62915474dbaf4
Marcin Chrost <mchrost82@gmail.com>
2022-10-16 23:09:50 +0200

    Initial revision

---
+++ rx-booking/src/main/java/eu/chrost/rxbooking/RxBookingService.java
@@ -0,0 +1,18 @@
+package eu.chrost.rxbooking;
+
+import java.time.Duration;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@Slf4j
+public class RxBookingService {
+    public Mono<String> book(String destination) {
+        return Mono.fromCallable(() -> destination)
+            .doOnNext(s -> log.info("[{}] Booking start", destination))
+            .delayElement(Duration.ofSeconds(3))
+            .map(d -> "Booked travel to: " + d)
+            .doOnNext(s -> log.info("[{}] Booking end", destination));
+    }
+}
