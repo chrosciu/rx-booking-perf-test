46ef2e8f734bbdf53593b027707ae1ed0edb5a94
Marcin Chrost <mchrost82@gmail.com>
2022-10-17 10:53:49 +0200

    BlockHound

--- rx-booking/src/main/java/com/chrosciu/rxbooking/RxBookingApplication.java
+++ rx-booking/src/main/java/com/chrosciu/rxbooking/RxBookingApplication.java
@@ -2,11 +2,13 @@ package com.chrosciu.rxbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class RxBookingApplication {
 
     public static void main(String[] args) {
+        BlockHound.install();
         SpringApplication.run(RxBookingApplication.class, args);
     }
 
