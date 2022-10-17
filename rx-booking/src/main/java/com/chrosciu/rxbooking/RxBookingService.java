362fcf9ef4addfe66c867b483e8b12b1327bdd47
Marcin Chrost <mchrost82@gmail.com>
2022-10-17 10:53:36 +0200

    Blocking Reactor thread
    (cherry picked from commit 8708bd5a32d30d6560892190f712f4c66e91b0c6)

--- rx-booking/src/main/java/com/chrosciu/rxbooking/RxBookingService.java
+++ rx-booking/src/main/java/com/chrosciu/rxbooking/RxBookingService.java
@@ -1,6 +1,6 @@
 package com.chrosciu.rxbooking;
 
-import java.time.Duration;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -24,8 +24,13 @@ public class RxBookingService {
     private Mono<String> book(String destination, TripType tripType) {
         return Mono.fromCallable(() -> destination)
             .doOnNext(s -> log.info("[{} {}] Booking start", destination, tripType))
-            .delayElement(Duration.ofSeconds(3))
-            .map(d -> String.format("Booked %s travel to: %s", tripType, d))
+            .map(d -> bookWithDelay(d, tripType))
             .doOnNext(s -> log.info("[{} {}] Booking end", destination, tripType));
+    }
+
+    @SneakyThrows
+    private String bookWithDelay(String destination, TripType tripType) {
+        Thread.sleep(3000);
+        return String.format("Booked %s travel to: %s", tripType, destination);
     }
 }
