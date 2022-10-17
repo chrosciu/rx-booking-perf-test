0be08292eddb683d99806e3d654ebb0ffd29a060
Marcin Chrost <mchrost82@gmail.com>
2022-10-17 09:48:22 +0200

    Back and there travel
    (cherry picked from commit 19d4bf4cb5c25af3ff96debaf2906da5cbe7ae97)

--- booking/src/main/java/com/chrosciu/booking/BookingService.java
+++ booking/src/main/java/com/chrosciu/booking/BookingService.java
@@ -2,18 +2,28 @@ package com.chrosciu.booking;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class BookingService {
-    @SneakyThrows
+    enum TripType {
+        THERE,
+        BACK
+    }
+
     public String book(String destination) {
-        log.info("[{}] Booking start", destination);
+        return String.join("\n",
+            book(destination, TripType.THERE),
+            book(destination, TripType.BACK)
+        );
+    }
+
+    @SneakyThrows
+    private String book(String destination, TripType tripType) {
+        log.info("[{} {}] Booking start", destination, tripType);
         Thread.sleep(3000);
-        log.info("[{}] Booking end", destination);
-        return "Booked travel to: " + destination;
+        log.info("[{} {}] Booking end", destination, tripType);
+        return String.format("Booked %s travel to:  %s", tripType, destination);
     }
 }
--- rx-booking/src/main/java/com/chrosciu/rxbooking/RxBookingService.java
+++ rx-booking/src/main/java/com/chrosciu/rxbooking/RxBookingService.java
@@ -8,11 +8,24 @@ import reactor.core.publisher.Mono;
 @Service
 @Slf4j
 public class RxBookingService {
+    enum TripType {
+        THERE,
+        BACK
+    }
+
     public Mono<String> book(String destination) {
+        return Mono.zip(
+            book(destination, TripType.THERE),
+            book(destination, TripType.BACK),
+            (there, back) -> String.join("\n", there, back)
+        );
+    }
+
+    private Mono<String> book(String destination, TripType tripType) {
         return Mono.fromCallable(() -> destination)
-            .doOnNext(s -> log.info("[{}] Booking start", destination))
+            .doOnNext(s -> log.info("[{} {}] Booking start", destination, tripType))
             .delayElement(Duration.ofSeconds(3))
-            .map(d -> "Booked travel to: " + d)
-            .doOnNext(s -> log.info("[{}] Booking end", destination));
+            .map(d -> String.format("Booked %s travel to: %s", tripType, d))
+            .doOnNext(s -> log.info("[{} {}] Booking end", destination, tripType));
     }
 }
