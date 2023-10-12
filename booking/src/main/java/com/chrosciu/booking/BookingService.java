24c2e5087a4b9df1976e81106b8bd11487f47865
Marcin Chrost <mchrost82@gmail.com>
2023-10-12 20:44:16 +0200

    Use blocking code also in Reactor streams generation. Created own schedulers as one provided by Reactor does not fit my needs

--- booking/src/main/java/com/chrosciu/booking/BookingService.java
+++ booking/src/main/java/com/chrosciu/booking/BookingService.java
@@ -1,10 +1,13 @@
 package com.chrosciu.booking;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 
-import java.time.Duration;
+import java.util.concurrent.ThreadFactory;
 
 @Service
 @Slf4j
@@ -14,20 +17,51 @@ public class BookingService {
         BACK
     }
 
+    /*
+        Default boundedElastic() scheduler behaves like a hybrid
+        - it creates virtual threads but is capped like they would have been platform ones
+        (10 * number of cores)
+        It is possible to change these settings via system properties,
+        however I decided just to create my two schedulers and use them alternatively`
+    */
+
+    private final ThreadFactory virtualThreadFactory = Thread.ofVirtual()
+            .name("prefix", 0)
+            .factory();
+
+    private final ThreadFactory platformThreadFactory = Thread.ofPlatform()
+            .name("prefix", 0)
+            .factory();
+
+
+    private final Scheduler virtualScheduler = Schedulers
+            .newBoundedElastic(1_000_000, 1_000_000, virtualThreadFactory, 60);
+
+    private final Scheduler platformScheduler = Schedulers
+            .newBoundedElastic(160, 1_000_000, platformThreadFactory, 60);
+
+    private final Scheduler scheduler = virtualScheduler;
+    //private final Scheduler scheduler = platformScheduler;
+
     public Mono<String> book(String destination) {
         return Mono.zip(
-                book(destination, TripType.THERE),
-                book(destination, TripType.BACK),
+                book(destination, TripType.THERE).subscribeOn(scheduler),
+                book(destination, TripType.BACK).subscribeOn(scheduler),
                 (there, back) -> String.join("\n", there, back)
         );
     }
 
     private Mono<String> book(String destination, TripType tripType) {
-        return Mono.fromCallable(() -> destination)
-                .doOnNext(s -> log.info("[{} {}] Booking start", destination, tripType))
-                .delayElement(Duration.ofSeconds(6))
-                .map(d -> String.format("Booked %s travel to: %s", tripType, d))
+        return Mono.fromCallable(() -> bookInternal(destination, tripType))
+                .doOnSubscribe(s -> log.info("[{} {}] Booking start", destination, tripType))
                 .doOnNext(s -> log.info("[{} {}] Booking end", destination, tripType));
+    }
+
+    @SneakyThrows
+    private String bookInternal(String destination, TripType tripType) {
+        Thread.sleep(6000);
+        log.info("{}", Thread.currentThread().isVirtual());
+        return String.format("Booked %s travel to: %s", tripType, destination);
     }
 
 }
